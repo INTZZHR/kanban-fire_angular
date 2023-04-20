093631102cdc096d203481336005bcfad25b75fc
INTZZHR <insblackzd@gmail.com>
2023-04-20 13:08:15 -0300

    -q

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kanbanfire-8486e',
+    appId: '1:502876372108:web:55f4049b0e88e2786bcfa9',
+    storageBucket: 'kanbanfire-8486e.appspot.com',
+    apiKey: 'AIzaSyBhnDMBqUap2I0apzrElE50gM5znq4ffls',
+    authDomain: 'kanbanfire-8486e.firebaseapp.com',
+    messagingSenderId: '502876372108',
+    measurementId: 'G-5RFJ3C24RK',
+  },};
